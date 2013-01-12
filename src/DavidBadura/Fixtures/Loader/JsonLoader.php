c146b58e2aa8f0e0443050203adf55739fa94ddc
DavidBadura <d.badura@gmx.de>
2013-01-12 12:07:22 +0100

    add json loader

---
+++ src/DavidBadura/Fixtures/Loader/JsonLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DavidBadura\Fixtures\Loader;
+
+use DavidBadura\Fixtures\FixtureCollection;
+use Symfony\Component\Finder\Finder;
+
+/**
+ *
+ * @author David Badura <d.badura@gmx.de>
+ */
+class JsonLoader implements LoaderInterface
+{
+
+    /**
+     *
+     * @param  mixed     $path
+     * @return FixtureCollection
+     */
+    public function load($path)
+    {
+        $finder = new Finder();
+        $finder->in($path)->name('*.json');
+
+        $fixtures = array();
+        foreach ($finder->files() as $file) {
+            $data = json_decode($file->getPathname(), true);
+            $fixtures = array_merge_recursive($fixtures, $data);
+        }
+
+        $collection = FixtureCollection::create($fixtures);
+        return $collection;
+    }
+
+}
